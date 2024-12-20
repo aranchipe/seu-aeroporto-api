a1477ad691534e3f6ea4733419c31ef273456273
aranchipe <aranchipe98@gmail.com>
2024-12-20 08:29:00 -0300

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-
-import menuRoutes from './routes/menuRoutes';
-import entidadeRoutes from './routes/entidadeRoutes';
+import express, { Request, Response } from 'express';
+import menuRouter from './routes/menuRoutes';
+import entidadeRouter from './routes/entidadeRoutes';
 import dynamicRoutes from './routes/dynamicRoutes';
 
 const app = express();
 
-// Middleware
-app.use(cors());
+// Middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Rotas
-app.use('/', menuRoutes);
-app.use('/entidades', entidadeRoutes);
+app.use('/menu', menuRouter);
+app.use('/entidades', entidadeRouter);
 app.use('/entidades', dynamicRoutes);
 
+
+// Endpoint para rota padrão
+app.get('/', (req: Request, res: Response) => {
+    res.send('API funcionando!');
+});
+
+// Exporta o app para Vercel
 export default app;
